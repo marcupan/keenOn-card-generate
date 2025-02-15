f33d4f608fa7ea92a82c0d472b9ba16e26330c05
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2025-02-15 09:06:42 +0200

    production configuration fixes

---
+++ src/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './user.entity';
+export * from './card.entity';
+export * from './folder.entity';
+export * from './model.entity';
--- src/utils/dataSource.ts
+++ src/utils/dataSource.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import config from 'config';
 import { DataSource } from 'typeorm';
 
+import '../entities';
+
 const postgresConfig = config.get<{
 	host: string;
 	port: number;
@@ -16,7 +18,16 @@ export const AppDataSource = new DataSource({
 	type: 'postgres',
 	synchronize: false,
 	logging: false,
-	entities: ['src/entities/**/*.entity{.ts,.js}'],
-	migrations: ['src/migrations/**/*{.ts,.js}'],
-	subscribers: ['src/subscribers/**/*{.ts,.js}'],
+	entities:
+		process.env.NODE_ENV === 'production'
+			? ['dist/src/entities/**/*.entity.js']
+			: ['src/entities/**/*.entity.{ts,js}'],
+	migrations:
+		process.env.NODE_ENV === 'production'
+			? ['dist/src/migrations/**/*.js']
+			: ['src/migrations/**/*.{ts,js}'],
+	subscribers:
+		process.env.NODE_ENV === 'production'
+			? ['dist/src/subscribers/**/*.js']
+			: ['src/subscribers/**/*.{ts,js}'],
 });
